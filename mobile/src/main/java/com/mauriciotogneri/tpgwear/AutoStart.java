f7425c15892847da491b74a3501d6b5ad8de050a
Mauricio Togneri <mauricio.togneri@gmail.com>
2015-09-18 22:27:10 +0200

    Updated icons

---
+++ mobile/src/main/java/com/mauriciotogneri/tpgwear/AutoStart.java
@@ -0,0 +1,14 @@
+package com.mauriciotogneri.tpgwear;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AutoStart extends BroadcastReceiver
+{
+    public void onReceive(Context context, Intent intent)
+    {
+        Intent intentService = new Intent(context, WearableService.class);
+        context.startService(intentService);
+    }
+}
